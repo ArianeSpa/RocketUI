eb87703677bcd4c4aed8e029898185ad057183c2
Ariane Spanneut <aspanneut.pro@gmail.com>
2024-11-12 23:08:35 +0100

    feat: init project with ract, typescript, styled-components, storybook, rollup storybook starts successfully, no stories to shwo for the moment can't build with rollup as there is no src

---
+++ scripts/remove-published-version.js
@@ -0,0 +1,34 @@
+const inquirer = require("inquirer");
+const { execSync } = require("child_process");
+
+const question = [
+  {
+    name: "version",
+    message:
+      "Which version (format X.X.X) of 'roket-ui' package would you like to unpublish ?",
+  },
+];
+
+inquirer.prompt(question).then((answers) => {
+  const isValid = RegExp(/\d*\.\d*\.\d*/gm).exec(answers.version);
+  if (!isValid) {
+    return console.log(
+      "Wrong format, version must be x.x.x for example 1.20.3"
+    );
+  }
+  inquirer
+    .prompt([
+      {
+        name: "confirm",
+        message: `Do you really want to unpublish rocket-ui@${answers.version} ? `,
+        type: "confirm",
+      },
+    ])
+    .then((confirmAnswers) => {
+      if (confirmAnswers.confirm) {
+        execSync(`npm unpublish react-lib-ui-test@${answers.version}`, {
+          stdio: "inherit",
+        });
+      }
+    });
+});
