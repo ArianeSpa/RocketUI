b67ff42482666eca661e4a40fe433bcfdd826b4d
Ariane Spanneut <aspanneut.pro@gmail.com>
2024-11-13 09:44:40 +0100

    feat: prettier

--- src/components/atoms/FakeAtom/FakeAtom.tsx
+++ src/components/atoms/FakeAtom/FakeAtom.tsx
@@ -1,8 +1,8 @@
 // === Import : NPM
-import React from "react";
+import React from 'react';
 
 // === Import : Local
-import { FakeAtomProps } from "./FakeAtom.types";
+import { FakeAtomProps } from './FakeAtom.types';
 
 const FakeAtom: React.FC<FakeAtomProps> = () => <div>Fake Atom</div>;
 export default FakeAtom;
